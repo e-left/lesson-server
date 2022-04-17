26114cb11efb4c59f8ac61e12f89901b6cadf7d0
Fotis Alexandridis <eleft@protonmail.com>
2022-04-17 19:34:27 +0300

    Updated the db.sql file

--- db.sql
+++ db.sql
@@ -228,3 +228,72 @@ CREATE TABLE IF NOT EXISTS maths_curriculum (
     main_content MEDIUMTEXT NOT NULL,
     FOREIGN KEY (userid) REFERENCES user(id)
 );
+
+CREATE TABLE IF NOT EXISTS chem_element (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    taksh INT NOT NULL,
+    typos VARCHAR(30) NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_sos_theory (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    taksh INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_book_chapters (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    taksh INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_book_exercises (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    taksh INT NOT NULL,
+    exNumber INT NOT NULL,
+    pageNumber INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_types (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    taksh INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_basic_appendix (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    taksh INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chem_curriculum (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid INT,
+    stamp_updated TIMESTAMP DEFAULT NOW() ON UPDATE NOW() NOT NULL,
+    taksh INT NOT NULL,
+    main_content MEDIUMTEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
